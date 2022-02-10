9b6518cae63f2c2021799bf15552a7f370eb950b
Yana <yanaegorova69.3@gmail.com>
2022-02-11 01:49:26 +0200

    +

---
+++ js/data/products.js
@@ -0,0 +1,380 @@
+export const products = [
+    {
+        id: 'product-1',
+        count: 'item1',
+        name: 'Sticker',
+        price: 0.99,
+        image: './img/img0.png',
+        description: [
+            {
+                text: 'Motivational Sticker to get you throughout the day!',
+            },
+            {
+                text: '3 x 3 inches',
+            },
+        ],
+        images: [],
+        isItRing: false,
+        ringSizes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'],
+    },
+    {
+        id: 'product-2',
+        count: 'item2',
+        name: 'Product Title',
+        price: 5.95,
+        image: './img/img1.png',
+        description: [
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+        ],
+        images: [],
+        isItRing: false,
+        ringSizes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'],
+    },
+    {
+        id: 'product-3',
+        count: 'item3',
+        name: 'Product Title',
+        price: 19.99,
+        image: './img/img2.png',
+        description: [
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+        ],
+        images: [],
+        isItRing: false,
+        ringSizes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'],
+    },
+    {
+        id: 'product-4',
+        count: 'item4',
+        name: 'Product Title',
+        price: 24.99,
+        image: './img/img3.png',
+        description: [
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+           
+        ],
+        images: [],
+        isItRing: false,
+        ringSizes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'],
+    },
+    {
+        id: 'product-5',
+        count: 'item5',
+        name: 'Product Title',
+        price: 39.99,
+        image: './img/img4.png',
+        description: [
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+         
+        ],
+        images: [],
+        isItRing: false,
+        ringSizes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'],
+    },
+    {
+        id: 'product-6',
+        count: 'item6',
+        name: 'Product Title',
+        price: 49.95,
+        image: './img/img5.png',
+        description: [
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+         
+        ],
+        images: [],
+        isItRing: false,
+        ringSizes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'],
+    },
+    {
+        id: 'product-7',
+        count: 'item7',
+        name: 'Product Title',
+        price: 89.99,
+        image: './img/img6.png',
+        description: [
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+         
+        ],
+        images: [],
+        isItRing: false,
+        ringSizes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'],
+    },
+    {
+        id: 'product-8',
+        count: 'item8',
+        name: 'Product Title',
+        price: 106.58,
+        image: './img/img7.png',
+        description: [
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+        ],
+        images: [],
+        isItRing: false,
+        ringSizes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'],
+    },
+    {
+        id: 'product-9',
+        count: 'item9',
+        name: 'Product Title',
+        price: 109.00,
+        image: './img/img8.png',
+        description: [
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+          
+        ],
+        images: [],
+        isItRing: false,
+        ringSizes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'],
+    },
+    {
+        id: 'product-10',
+        count: 'item10',
+        name: 'Product Title',
+        price: 115.89,
+        image: './img/img9.png',
+        description: [
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+          
+        ],
+        images: [],
+        isItRing: false,
+        ringSizes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'],
+    },
+    {
+        id: 'product-11',
+        count: 'item11',
+        name: 'Product Title',
+        price: 119.00,
+        image: './img/img10.png',
+        description: [
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+          
+        ],
+        images: [],
+        isItRing: false,
+        ringSizes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'],
+    },
+    {
+        id: 'product-12',
+        count: 'item12',
+        name: 'Product Title',
+        price: 122.00,
+        image: './img/img11.png',
+        description: [
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+         
+        ],
+        images: [],
+        isItRing: false,
+        ringSizes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'],
+    },
+    {
+        id: 'product-13',
+        count: 'item13',
+        name: 'Product Title',
+        price: 124.53,
+        image: './img/img12.png',
+        description: [
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+         
+        ],
+        images: [],
+        isItRing: false,
+        ringSizes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'],
+    },
+    {
+        id: 'product-14',
+        count: 'item14',
+        name: 'Product Title',
+        price: 129.87,
+        image: './img/img13.png',
+        description: [
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+        ],
+        images: [],
+        isItRing: false,
+        ringSizes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'],
+    },
+]
